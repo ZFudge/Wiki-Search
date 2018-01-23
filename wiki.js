7079fc6001710db4854ab9af0018db8da9342782
zfudge <zacheryfudge@gmail.com>
2018-01-23 07:57:58 -0700

    added origin header, CORS working

--- wiki.js
+++ wiki.js
@@ -3,13 +3,38 @@ const wiki = {
 	container: document.getElementById('container'),
 	focus: function() {
 		wiki.search.style.width = '60vw';
-		wiki.container.style.marginLeft = '-30vw';
 	},
 	unfocus: function() {
 		wiki.search.style.width = '30vw';
-		wiki.container.style.marginLeft = '-15vw';
+	},
+	requestSearch: function(searchTerm) {
+		console.log('called');
+		const xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === XMLHttpRequest.DONE) {
+				wiki.showResults(JSON.parse(xhr.response));
+			}
+		}
+		xhr.open("GET", `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${searchTerm}&limit=10&origin=*&suggest=1`);
+		xhr.send();
+	},
+	showResults: function(arr) {
+		const newArr = [];
+		for(let i = 0; i < arr[1].length; i++) {
+			console.log(arr[1][i],arr[2][i],arr[3][i]);
+			newArr.push(`<p><span>${arr[1][i]}</span><a target='_blank' href="${arr[3][i]}">${arr[2][i]}</a>`);
+			const p = document.createElement('p');
+			p.innerHTML = newArr[i];
+			document.getElementById('results').appendChild(p);
+		}
 	}
 };
+
 wiki.search.addEventListener("focusin", wiki.focus);
 wiki.search.addEventListener("focusout", wiki.unfocus);
+wiki.search.addEventListener("keydown", function(btn) {
+	if (btn.keyCode == 13) wiki.requestSearch(wiki.search.value);
+});
+
 
